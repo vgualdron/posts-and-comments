5278b6f414030f2dd9770711fe3c59ba7a2a2d8a
vgualdron <victor.gualdron.r@gmail.com>
2021-03-24 01:00:50 -0500

    ajustar paths para compilar app

--- config/env.config.js
+++ config/env.config.js
@@ -1,6 +1,6 @@
 const production = {
     appName: 'Posts and Comments', // Nombre de la aplicación html - title
-    appBaseDir: './', // Directorio de despliegue de la aplicación
+    appBaseDir: '/posts-and-comments', // Directorio de despliegue de la aplicación
     rootUrl: '../../../', // Url del servidor del api
     baseUrl: '../../../', //Url del apí por default
   }
